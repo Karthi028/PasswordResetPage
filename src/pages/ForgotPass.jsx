5223917393ecc76cecee329eabc8d85f94474b68
karthi <karthijayasangar6564@gmail.com>
2025-08-21 13:48:34 +0530

    updated

--- src/pages/ForgotPass.jsx
+++ src/pages/ForgotPass.jsx
@@ -17,23 +17,29 @@ const ForgotPass = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body:JSON.stringify(useremail)
+            body: JSON.stringify(useremail)
         })
-        .then((response)=>{
-            return response.json();
-        })
-        .then((data)=>{
-            console.log(data)
-            alert("Mail sent Successfully")
-            const tag = document.getElementById('Note');
-            tag.classList.remove('hidden');
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
 
-        })
-        .catch((err)=>{
-            console.log(err.message);
-            const tag = document.getElementById('Noted');
-            tag.classList.remove('hidden');
-        })
+                } else if (response.status === 404) {
+                    throw new Error('Server responded with Status:${response.status')
+                }
+            })
+            .then((data) => {
+                console.log(data)
+                alert("Mail sent Successfully")
+                const tag = document.getElementById('Note');
+                const tags = document.getElementById('Noted');
+                tags.classList.add('hidden');
+                tag.classList.remove('hidden');
+
+            })
+            .catch((err) => {
+                const tag = document.getElementById('Noted');
+                tag.classList.remove('hidden');
+            })
     }
 
     return <div className="p-4 flex justify-center mt-20 ml-10 mr-10 sm:ml-[25%] sm:mr-[25%] md:ml-[35%] md:mr-[35%] border rounded-2xl border-cyan-200">
@@ -44,7 +50,8 @@ const ForgotPass = () => {
             <button className="p-1 bg-cyan-200 text-white font-bold rounded-full">Send Email</button>
             <button className="absolute top-[-30px] left-[10%] right-[10%] text-sm p-1 bg-lime-300 rounded-full text-white font-semibold">Forgot Your Password</button>
             <p id="Note" className="text-[10px] text-center hidden text-red-500">Check your Mail & click the reset password Link </p>
-            <p id="Noted" className="text-xs text-center hidden text-red-500">Issue with the Server try after sometime!!!</p>
+            <p id="Noted" className="text-xs text-center hidden text-red-500">Issue with the Server try after sometime..</p>
+
         </form>
     </div>
 }
